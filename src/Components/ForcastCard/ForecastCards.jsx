c3f88c6477f68516fa93112d52ae90e21c571229
ahmedsayed270 <22ahmed23sayed24@gmail.com>
2025-07-17 06:54:26 +0300

    first deploy

---
+++ src/Components/ForcastCard/ForecastCards.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { motion } from "framer-motion";
+import img2 from "../../assets/img2.jpg";
+import rainImg from "../../assets/img3.jpg";
+
+export default function ForecastCards({ forecastDays }) {
+  return (
+    <div className="space-y-4">
+      {forecastDays?.map((day, index) => {
+        const bgImage = day.day.condition.text.toLowerCase().includes("rain")
+          ? rainImg
+          : img2;
+
+        return (
+          <motion.div
+            key={day.date}
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: index * 0.3, duration: 0.3 }}
+            className="p-4 rounded h-[80px] bg-cover bg-center bg-no-repeat shadow-lg transition-all flex justify-between hover:scale-105"
+            style={{ backgroundImage: `url(${bgImage})` }}
+          >
+            <div className="flex items-center gap-4">
+              <img
+                src={day.day.condition.icon}
+                alt="icon"
+                className="mx-auto w-[50px]"
+              />
+              <p>{day.day.avgtemp_c}°C</p>
+            </div>
+            <div>
+              <p className=" font-semibold">{day.date}</p>
+              <p className=" font-medium">{day.day.condition.text}</p>
+            </div>
+          </motion.div>
+        );
+      })}
+    </div>
+  );
+}
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,128 @@
+import React, { useState, useEffect } from "react";
+import img1 from "../../assets/img1.jpg";
+import img2 from "../../assets/img2.jpg";
+import rainImg from "../../assets/img3.jpg";
+import { useFormik } from "formik";
+import axios from "axios";
+import ForecastCards from "../ForcastCard/ForecastCards";
+
+export default function Home() {
+  const [BackGround, setBackGround] = useState(img1);
+  const [showResultDiv, setShowResultDiv] = useState(false);
+  const [weatherData, setWeatherData] = useState(null);
+  useEffect(() => {
+    weatherimg();
+  }, [weatherData]);
+  function weatherimg() {
+    if (!weatherData) return;
+    if (weatherData.current.temp_c <= 20) {
+      setBackGround(rainImg);
+    } else {
+      setBackGround(img2);
+    }
+  }
+  async function getWeatherApi(cityName) {
+    try {
+      const options = {
+        url: `https://api.weatherapi.com/v1/forecast.json?key=9bd493d555c44208bf652107252304&q=${cityName}&days=${3}`,
+        method: "GET",
+      };
+      const { data } = await axios.request(options);
+      setWeatherData(data);
+      setShowResultDiv(true);
+      handleChangeBackground();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+      console.log("error");
+    }
+  }
+
+  const formik = useFormik({
+    initialValues: {
+      city: "",
+    },
+    onSubmit: (values) => {
+      getWeatherApi(values.city);
+    },
+  });
+
+  function handleChangeBackground() {
+    setBackGround(img2);
+  }
+
+  const BodyImg = {
+    backgroundImage: `url(${BackGround})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    minHeight: "100vh",
+    transition: "all 1s",
+  };
+
+  return (
+    <div
+      style={BodyImg}
+      className="flex flex-col md:flex-row justify-center items-center gap-x-4 "
+    >
+      <div
+        className={` my-4 md:my-0 bg-white/20 backdrop-blur-md w-[450px] p-2 text-center space-y-5.5 rounded-2xl transition-all duration-700 ${
+          showResultDiv
+            ? "-translate-x-[0] md:-translate-x-[10%] "
+            : "translate-x-0 opacity-100"
+        }`}
+      >
+        <h1 className="text-xl font-semibold text-black/90">Weather</h1>
+        <h2 className="text-xl text-white font-semibold">
+          Find the weather of any city
+        </h2>
+        <p className="text-xl text-white font-semibold">
+          Enter the name of the city to get the weather
+        </p>
+        <form onSubmit={formik.handleSubmit}>
+          <input
+            className="text-black text-2xl border-b-2 border-blue-400 focus:outline-none w-[250px] text-center "
+            type="text"
+            name="city"
+            value={formik.values.city}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+          <div className="flex justify-center items-center">
+            <button
+              type="submit"
+              className="btn mt-4 bg-blue-500 text-white px-4 py-2 rounded "
+            >
+              Get Weather
+            </button>
+          </div>
+        </form>
+      </div>
+
+      {showResultDiv && weatherData && (
+        <>
+          <div className="bg-white/30 backdrop-blur-md rounded-xl p-6 text-center text-white space-y-4 transition-all duration-700 animate-fade-in w-[450px] animate__animated animate__backInRight animate__slow">
+            <h2 className="text-3xl font-bold text-black">
+              {weatherData.location.name}
+            </h2>
+            <img
+              src={weatherData.current.condition.icon}
+              alt="weather icon"
+              className="mx-auto w-[80px]"
+            />
+            <div className="space-y-1">
+              <p className="text-2xl">{weatherData.current.temp_c}°C -</p>
+              <p>{weatherData.current.condition.text}</p>
+            </div>
+          </div>
+          <div className="p-6 text-center  space-y-6 transition-all duration-700 animate-fade-in w-[450px]">
+            <ForecastCards
+              key={weatherData.location.name}
+              forecastDays={weatherData?.forecast?.forecastday}
+            />
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
